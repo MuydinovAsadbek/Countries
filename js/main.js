149659eab23cf5afaf03e282b10e83356ce6e507
MuydinovAsadbek <asadbekmoydinov06@gmail.com>
2022-04-12 17:03:23 +0500

    add main-content and styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const elDarkModeButton = document.querySelector('.button');
 
 elDarkModeButton.addEventListener('click', function () {
-  document.body.classList.add('dark-mode');
+  document.body.classList.toggle('dark-mode');
 });
