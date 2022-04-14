fc8e3f07f6dd14c9b6e7a5da9450794a1fe6b703
MuydinovAsadbek <asadbekmoydinov06@gmail.com>
2022-04-14 15:47:59 +0500

    Add page more and styles

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ const elDarkModeButton = document.querySelector('.button');
 elDarkModeButton.addEventListener('click', function () {
   document.body.classList.toggle('dark-mode');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const theme = localStorage.getItem("theme", "dark");
+  const darkModeToggle = document.getElementById("chk");
+
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark-mode");
+  } else {
+    document.documentElement.classList.remove("dark-mode");
+  }
+
+  darkModeToggle.addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark-mode");
+
+    if (document.documentElement.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
